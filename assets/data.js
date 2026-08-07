43e4d90125a13d0cf743e52203b95c48c0a53072
jojozmj-sys <jojozmj@icloud.com>
2026-08-07 15:09:50 +0800

    deploy (auto push)

--- assets/data.js
+++ assets/data.js
@@ -4163,7 +4163,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 1,
           "title": "泸溪河牙冠事件消费者已致歉",
-          "hot": 1233802,
+          "hot": 1215108,
           "url": "https://s.weibo.com/weibo?q=%E6%B3%B8%E6%BA%AA%E6%B2%B3%E7%89%99%E5%86%A0%E4%BA%8B%E4%BB%B6%E6%B6%88%E8%B4%B9%E8%80%85%E5%B7%B2%E8%87%B4%E6%AD%89",
           "tag": "社会",
           "desc": ""
@@ -4172,7 +4172,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 2,
           "title": "你上班银行上班你午休银行午休",
-          "hot": 1090398,
+          "hot": 1096680,
           "url": "https://s.weibo.com/weibo?q=%E4%BD%A0%E4%B8%8A%E7%8F%AD%E9%93%B6%E8%A1%8C%E4%B8%8A%E7%8F%AD%E4%BD%A0%E5%8D%88%E4%BC%91%E9%93%B6%E8%A1%8C%E5%8D%88%E4%BC%91",
           "tag": "社会",
           "desc": ""
@@ -4181,7 +4181,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 3,
           "title": "秋天第1个节气",
-          "hot": 827463,
+          "hot": 921604,
           "url": "https://s.weibo.com/weibo?q=%E7%A7%8B%E5%A4%A9%E7%AC%AC1%E4%B8%AA%E8%8A%82%E6%B0%94",
           "tag": "社会",
           "desc": ""
@@ -4190,7 +4190,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 4,
           "title": "张雅琪晒中餐厅10图",
-          "hot": 827408,
+          "hot": 916224,
           "url": "https://s.weibo.com/weibo?q=%E5%BC%A0%E9%9B%85%E7%90%AA%E6%99%92%E4%B8%AD%E9%A4%90%E5%8E%8510%E5%9B%BE",
           "tag": "社会",
           "desc": ""
@@ -4199,7 +4199,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 5,
           "title": "现在的小学生已经是庞然大物了",
-          "hot": 827357,
+          "hot": 883787,
           "url": "https://s.weibo.com/weibo?q=%E7%8E%B0%E5%9C%A8%E7%9A%84%E5%B0%8F%E5%AD%A6%E7%94%9F%E5%B7%B2%E7%BB%8F%E6%98%AF%E5%BA%9E%E7%84%B6%E5%A4%A7%E7%89%A9%E4%BA%86",
           "tag": "社会",
           "desc": ""
@@ -4207,18 +4207,18 @@ window.WORKBENCH_DATA = {
         {
           "platform": "微博",
           "rank": 6,
-          "title": "ai漫剧男主唯一塌房方式被找到了",
-          "hot": 827223,
-          "url": "https://s.weibo.com/weibo?q=ai%E6%BC%AB%E5%89%A7%E7%94%B7%E4%B8%BB%E5%94%AF%E4%B8%80%E5%A1%8C%E6%88%BF%E6%96%B9%E5%BC%8F%E8%A2%AB%E6%89%BE%E5%88%B0%E4%BA%86",
+          "title": "韩国被爆性贿赂世预赛裁判",
+          "hot": 805913,
+          "url": "https://s.weibo.com/weibo?q=%E9%9F%A9%E5%9B%BD%E8%A2%AB%E7%88%86%E6%80%A7%E8%B4%BF%E8%B5%82%E4%B8%96%E9%A2%84%E8%B5%9B%E8%A3%81%E5%88%A4",
           "tag": "社会",
           "desc": ""
         },
         {
           "platform": "微博",
           "rank": 7,
-          "title": "韩国被爆性贿赂世预赛裁判",
-          "hot": 810487,
-          "url": "https://s.weibo.com/weibo?q=%E9%9F%A9%E5%9B%BD%E8%A2%AB%E7%88%86%E6%80%A7%E8%B4%BF%E8%B5%82%E4%B8%96%E9%A2%84%E8%B5%9B%E8%A3%81%E5%88%A4",
+          "title": "ai漫剧男主唯一塌房方式被找到了",
+          "hot": 796038,
+          "url": "https://s.weibo.com/weibo?q=ai%E6%BC%AB%E5%89%A7%E7%94%B7%E4%B8%BB%E5%94%AF%E4%B8%80%E5%A1%8C%E6%88%BF%E6%96%B9%E5%BC%8F%E8%A2%AB%E6%89%BE%E5%88%B0%E4%BA%86",
           "tag": "社会",
           "desc": ""
         },
@@ -4226,7 +4226,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 8,
           "title": "星巴克 秋天第一场商战",
-          "hot": 718530,
+          "hot": 704045,
           "url": "https://s.weibo.com/weibo?q=%E6%98%9F%E5%B7%B4%E5%85%8B%20%E7%A7%8B%E5%A4%A9%E7%AC%AC%E4%B8%80%E5%9C%BA%E5%95%86%E6%88%98",
           "tag": "社会",
           "desc": ""
@@ -4235,7 +4235,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 9,
           "title": "于适 平儿都长这么大了",
-          "hot": 655318,
+          "hot": 650782,
           "url": "https://s.weibo.com/weibo?q=%E4%BA%8E%E9%80%82%20%E5%B9%B3%E5%84%BF%E9%83%BD%E9%95%BF%E8%BF%99%E4%B9%88%E5%A4%A7%E4%BA%86",
           "tag": "社会",
           "desc": ""
@@ -4244,7 +4244,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 10,
           "title": "提离职被知道要裁员的领导硬留下",
-          "hot": 627613,
+          "hot": 631467,
           "url": "https://s.weibo.com/weibo?q=%E6%8F%90%E7%A6%BB%E8%81%8C%E8%A2%AB%E7%9F%A5%E9%81%93%E8%A6%81%E8%A3%81%E5%91%98%E7%9A%84%E9%A2%86%E5%AF%BC%E7%A1%AC%E7%95%99%E4%B8%8B",
           "tag": "社会",
           "desc": ""
@@ -4253,7 +4253,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 11,
           "title": "好演员从不无缝进组",
-          "hot": 577831,
+          "hot": 577772,
           "url": "https://s.weibo.com/weibo?q=%E5%A5%BD%E6%BC%94%E5%91%98%E4%BB%8E%E4%B8%8D%E6%97%A0%E7%BC%9D%E8%BF%9B%E7%BB%84",
           "tag": "社会",
           "desc": ""
@@ -4262,7 +4262,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 12,
           "title": "戚薇AI短剧 雷霆视角",
-          "hot": 567369,
+          "hot": 549250,
           "url": "https://s.weibo.com/weibo?q=%E6%88%9A%E8%96%87AI%E7%9F%AD%E5%89%A7%20%E9%9B%B7%E9%9C%86%E8%A7%86%E8%A7%92",
           "tag": "社会",
           "desc": ""
@@ -4271,7 +4271,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 13,
           "title": "情绪冷漠症",
-          "hot": 521954,
+          "hot": 517919,
           "url": "https://s.weibo.com/weibo?q=%E6%83%85%E7%BB%AA%E5%86%B7%E6%BC%A0%E7%97%87",
           "tag": "社会",
           "desc": ""
@@ -4280,7 +4280,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 14,
           "title": "Mina同学发声",
-          "hot": 519170,
+          "hot": 516849,
           "url": "https://s.weibo.com/weibo?q=Mina%E5%90%8C%E5%AD%A6%E5%8F%91%E5%A3%B0",
           "tag": "社会",
           "desc": ""
@@ -4289,7 +4289,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 15,
           "title": "面试面出了吃火锅的感觉",
-          "hot": 516124,
+          "hot": 508570,
           "url": "https://s.weibo.com/weibo?q=%E9%9D%A2%E8%AF%95%E9%9D%A2%E5%87%BA%E4%BA%86%E5%90%83%E7%81%AB%E9%94%85%E7%9A%84%E6%84%9F%E8%A7%89",
           "tag": "社会",
           "desc": ""
@@ -4298,7 +4298,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 16,
           "title": "宜宾地震",
-          "hot": 501590,
+          "hot": 499598,
           "url": "https://s.weibo.com/weibo?q=%E5%AE%9C%E5%AE%BE%E5%9C%B0%E9%9C%87",
           "tag": "社会",
           "desc": ""
@@ -4307,7 +4307,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 17,
           "title": "BLACKPINK全员确认出席十周年",
-          "hot": 473810,
+          "hot": 475702,
           "url": "https://s.weibo.com/weibo?q=BLACKPINK%E5%85%A8%E5%91%98%E7%A1%AE%E8%AE%A4%E5%87%BA%E5%B8%AD%E5%8D%81%E5%91%A8%E5%B9%B4",
           "tag": "社会",
           "desc": ""
@@ -4316,7 +4316,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 18,
           "title": "百花奖",
-          "hot": 333375,
+          "hot": 346758,
           "url": "https://s.weibo.com/weibo?q=%E7%99%BE%E8%8A%B1%E5%A5%96",
           "tag": "社会",
           "desc": ""
@@ -4325,7 +4325,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 19,
           "title": "泰国校园枪击已致7死",
-          "hot": 332778,
+          "hot": 346431,
           "url": "https://s.weibo.com/weibo?q=%E6%B3%B0%E5%9B%BD%E6%A0%A1%E5%9B%AD%E6%9E%AA%E5%87%BB%E5%B7%B2%E8%87%B47%E6%AD%BB",
           "tag": "社会",
           "desc": ""
@@ -4334,7 +4334,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 20,
           "title": "1岁宝宝碰坏纸巾盒三亚酒店索赔924元",
-          "hot": 325431,
+          "hot": 345950,
           "url": "https://s.weibo.com/weibo?q=1%E5%B2%81%E5%AE%9D%E5%AE%9D%E7%A2%B0%E5%9D%8F%E7%BA%B8%E5%B7%BE%E7%9B%92%E4%B8%89%E4%BA%9A%E9%85%92%E5%BA%97%E7%B4%A2%E8%B5%94924%E5%85%83",
           "tag": "社会",
           "desc": ""
@@ -4342,18 +4342,18 @@ window.WORKBENCH_DATA = {
         {
           "platform": "微博",
           "rank": 21,
-          "title": "老中医提醒立秋一定要管住嘴",
-          "hot": 322683,
-          "url": "https://s.weibo.com/weibo?q=%E8%80%81%E4%B8%AD%E5%8C%BB%E6%8F%90%E9%86%92%E7%AB%8B%E7%A7%8B%E4%B8%80%E5%AE%9A%E8%A6%81%E7%AE%A1%E4%BD%8F%E5%98%B4",
+          "title": "女儿卖房定价9000被母亲7500签约",
+          "hot": 344687,
+          "url": "https://s.weibo.com/weibo?q=%E5%A5%B3%E5%84%BF%E5%8D%96%E6%88%BF%E5%AE%9A%E4%BB%B79000%E8%A2%AB%E6%AF%8D%E4%BA%B27500%E7%AD%BE%E7%BA%A6",
           "tag": "社会",
           "desc": ""
         },
         {
           "platform": "微博",
           "rank": 22,
-          "title": "女儿卖房定价9000被母亲7500签约",
-          "hot": 314543,
-          "url": "https://s.weibo.com/weibo?q=%E5%A5%B3%E5%84%BF%E5%8D%96%E6%88%BF%E5%AE%9A%E4%BB%B79000%E8%A2%AB%E6%AF%8D%E4%BA%B27500%E7%AD%BE%E7%BA%A6",
+          "title": "老中医提醒立秋一定要管住嘴",
+          "hot": 343710,
+          "url": "https://s.weibo.com/weibo?q=%E8%80%81%E4%B8%AD%E5%8C%BB%E6%8F%90%E9%86%92%E7%AB%8B%E7%A7%8B%E4%B8%80%E5%AE%9A%E8%A6%81%E7%AE%A1%E4%BD%8F%E5%98%B4",
           "tag": "社会",
           "desc": ""
         },
@@ -4361,7 +4361,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 23,
           "title": "人大教授花35万公款考证莫言家世",
-          "hot": 309343,
+          "hot": 343541,
           "url": "https://s.weibo.com/weibo?q=%E4%BA%BA%E5%A4%A7%E6%95%99%E6%8E%88%E8%8A%B135%E4%B8%87%E5%85%AC%E6%AC%BE%E8%80%83%E8%AF%81%E8%8E%AB%E8%A8%80%E5%AE%B6%E4%B8%96",
           "tag": "社会",
           "desc": ""
@@ -4369,9 +4369,9 @@ window.WORKBENCH_DATA = {
         {
           "platform": "微博",
           "rank": 24,
-          "title": "戚薇 授权AI",
-          "hot": 304053,
-          "url": "https://s.weibo.com/weibo?q=%E6%88%9A%E8%96%87%20%E6%8E%88%E6%9D%83AI",
+          "title": "吴倩女儿长这么大了",
+          "hot": 342673,
+          "url": "https://s.weibo.com/weibo?q=%E5%90%B4%E5%80%A9%E5%A5%B3%E5%84%BF%E9%95%BF%E8%BF%99%E4%B9%88%E5%A4%A7%E4%BA%86",
           "tag": "社会",
           "desc": ""
         },
@@ -4379,7 +4379,7 @@ window.WORKBENCH_DATA = {
           "platform": "微博",
           "rank": 25,
           "title": "云南茶园发现眼镜王蛇一家38口",
-          "hot": 300203,
+          "hot": 341193,
           "url": "https://s.weibo.com/weibo?q=%E4%BA%91%E5%8D%97%E8%8C%B6%E5%9B%AD%E5%8F%91%E7%8E%B0%E7%9C%BC%E9%95%9C%E7%8E%8B%E8%9B%87%E4%B8%80%E5%AE%B638%E5%8F%A3",
           "tag": "社会",
           "desc": ""
@@ -4901,7 +4901,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 8,
           "title": "当和你一起玩的那个兄弟延迟过高：",
-          "hot": 4899395,
+          "hot": 4902025,
           "url": "https://b23.tv/BV1JQuc6fEsA",
           "tag": "影视",
           "desc": "-"
@@ -4919,7 +4919,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 10,
           "title": "柯洁VS党毅飞（不可能的逆转）",
-          "hot": 5030101,
+          "hot": 5031007,
           "url": "https://b23.tv/BV1kxuw6iEb2",
           "tag": "影视",
           "desc": ""
@@ -4928,7 +4928,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 11,
           "title": "看完7月新番，我走火入魔！浑身燥热！【泛式】",
-          "hot": 1454160,
+          "hot": 1454391,
           "url": "https://b23.tv/BV1tcMy6fEEU",
           "tag": "影视",
           "desc": "这就是7月新番的大火猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！2026年七月新番的猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！猛炒！爆炒！狂炒！ 素材引用 宿敌美声 BV1Nm421K7px 游戏玩家 BV1E1KH6CEhk 巧虎唱歌 BV1USQUYsEMD"
@@ -4937,7 +4937,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 12,
           "title": "美利坚糖门黄油大胃袋VS国产味真族良子板面长老",
-          "hot": 3720736,
+          "hot": 3720874,
           "url": "https://b23.tv/BV1JmMf69EZY",
           "tag": "影视",
           "desc": ""
@@ -4964,7 +4964,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 15,
           "title": "本想简单吃个见手青小炒，却被云南厨师火焰魔法吓晕了...",
-          "hot": 2789088,
+          "hot": 2790119,
           "url": "https://b23.tv/BV1e8Mk6wEWZ",
           "tag": "影视",
           "desc": "这是我吃了见手青之后看到的幻觉吗？"
@@ -4973,7 +4973,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 16,
           "title": "当我梦到了一段很锈湖的音乐。",
-          "hot": 3285092,
+          "hot": 3285986,
           "url": "https://b23.tv/BV1eKMX6tERP",
           "tag": "影视",
           "desc": "当我梦到了一段很锈湖的音乐…"
@@ -4991,7 +4991,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 18,
           "title": "大家还想看我搬空什么店",
-          "hot": 8715145,
+          "hot": 8715937,
           "url": "https://b23.tv/BV1bz3Q6oEMP",
           "tag": "影视",
           "desc": "-"
@@ -5000,7 +5000,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 19,
           "title": "有些善意当时没有声音，但总有一天，你会听到它的回响。",
-          "hot": 1614475,
+          "hot": 1614727,
           "url": "https://b23.tv/BV1BG3f6bExg",
           "tag": "影视",
           "desc": "以前都是小唐把热饼递给这位黑人小哥，这一次，他却抱着一大瓶饮料专程回来。小哥说，自从这里开始免费送饼，街区不少人在最困难的时候，至少还能吃上一口热的。但这份感谢不该只属于小唐，因为每一张送出去的饼，背后还有很多人的共同支持。"
@@ -5027,7 +5027,7 @@ window.WORKBENCH_DATA = {
           "platform": "B站",
           "rank": 22,
           "title": "非遗文化博大精深，非遗文化燃起来了#万物皆可跳梦的翅膀＃梦的翅膀受了伤",
-          "hot": 3703758,
+          "hot": 3704663,
           "url": "https://b23.tv/BV1cxuF6ZEq3",
           "tag": "影视",
           "desc": "-"
